d739ab408270438b8566a65d6092b61c5e98fd06
BYRNE <kevinbyrnety@gmail.com>
2014-10-22 17:15:25 +0100

    added styling to carts page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022111030) do
+ActiveRecord::Schema.define(version: 20141022113728) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
